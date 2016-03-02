4556f785173046a9d1774f646192b756bc2d8f91
Daniel Holmlund <agnathan@gmail.com>
2016-03-02 10:55:43 -0800

    Adding the site

---
+++ app/scripts/config.js
@@ -0,0 +1,108 @@
+/**
+ * INSPINIA - Responsive Admin Theme
+ *
+ * Inspinia theme use AngularUI Router to manage routing and views
+ * Each view are defined as state.
+ * Initial there are written stat for all view in theme.
+ *
+ */
+function config($stateProvider, $urlRouterProvider, $ocLazyLoadProvider) {
+  $urlRouterProvider.otherwise("/index/home");
+
+  $ocLazyLoadProvider.config({
+    // Set to true if you want to see what and when is dynamically loaded
+    debug: false
+  });
+
+  $stateProvider
+
+    .state('index', {
+      abstract: true,
+      url: "/index",
+      templateUrl: "views/common/content.html",
+    })
+    .state('index.home', {
+      url: "/home",
+      templateUrl: "views/home.html",
+      data: { pageTitle: 'Home' }
+    })
+    .state('index.presentations', {
+      url: "/presentations",
+      templateUrl: "views/presentations.html",
+      data: { pageTitle: 'Slides View' }
+    })
+    .state('labs', {
+      abstract: true,
+      url: "/labs",
+      templateUrl: "views/common/content.html"
+    })
+    .state('labs.introduction', {
+      url: "/introduction",
+      templateUrl: "views/labs/intro/index.html",
+      data: { pageTitle: 'Introduction' }
+    })
+    .state('labs.build', {
+      url: "/edge-device",
+      templateUrl: "views/labs/edge-device/index.html",
+      data: { pageTitle: 'Build an Edge Device' },
+      resolve: {
+        loadPlugin: function ($ocLazyLoad) {
+          return $ocLazyLoad.load([
+            {
+              serie: true,
+              files: [
+                'bower_components/codemirror/lib/codemirror.css',
+                'bower_components/codemirror/theme/ambiance.css',
+                'bower_components/codemirror/lib/codemirror.js',
+                'bower_components/codemirror/mode/javascript/javascript.js'
+              ]
+            },
+            {
+              name: 'ui.codemirror',
+              files: ['bower_components/angular-ui-codemirror/ui-codemirror.min.js']
+            }
+          ]);
+        }
+      }
+    })
+    .state('labs.helix', {
+      url: "/helix",
+      templateUrl: "views/labs/hdc/index.html",
+      data: { pageTitle: 'Helix Device Cloud' }
+    })
+    .state('labs.security', {
+      url: "/security",
+      templateUrl: "views/labs/security/index.html",
+      data: { pageTitle: 'Security on the Internet of Things' },
+      resolve: {
+        loadPlugin: function ($ocLazyLoad) {
+          return $ocLazyLoad.load([
+            {
+              serie: true,
+              files: [
+                'bower_components/codemirror/lib/codemirror.css',
+                'bower_components/codemirror/theme/ambiance.css',
+                'bower_components/codemirror/lib/codemirror.js',
+                'bower_components/codemirror/mode/javascript/javascript.js'
+              ]
+            },
+            {
+              name: 'ui.codemirror',
+              files: ['bower_components/angular-ui-codemirror/ui-codemirror.min.js']
+            }
+          ]);
+        }
+      }
+    })
+    .state('labs.introduction3', {
+      url: "/introduction3",
+      templateUrl: "views/labs/introduction/index3.html",
+      data: { pageTitle: 'Introduction3' }
+    })
+}
+angular
+  .module('inspinia')
+  .config(config)
+  .run(function($rootScope, $state) {
+    $rootScope.$state = $state;
+  });
---
+++ app/scripts/controllers.js
@@ -0,0 +1,34 @@
+/**
+ * INSPINIA - Responsive Admin Theme
+ *
+ */
+
+/**
+ * MainCtrl - controller
+ */
+function MainCtrl() {
+
+    this.userName = 'Guest';
+    this.helloText = 'Welcome to the Intel Commercial IoT Workshop';
+    this.descriptionText = '';
+
+};
+
+
+/**
+ * codeEditorCtrl - Controller for code editor - Code Mirror
+ */
+function CodeEditorCtrl($scope) {
+  $scope.editorOptions = {
+    lineNumbers: true,
+    matchBrackets: true,
+    styleActiveLine: true,
+    theme:"ambiance"
+  };
+}
+
+
+angular
+    .module('inspinia')
+  .controller('MainCtrl', MainCtrl)
+  .controller('CodeEditorCtrl', CodeEditorCtrl)
