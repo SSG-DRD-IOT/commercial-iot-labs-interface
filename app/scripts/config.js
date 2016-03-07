b53126586d1d97394fc6a686d4bacad9cfddabdc
Daniel Holmlund <agnathan@gmail.com>
2016-03-07 12:43:55 -0800

    Added the Data Analytics Lab

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -224,6 +224,30 @@ function config($stateProvider, $urlRouterProvider, $ocLazyLoadProvider) {
         }
       }
     })
+    .state('labs.cloud', {
+      url: "/cloud",
+      templateUrl: "views/labs/cloud/index.html",
+      data: { pageTitle: 'Data Analytics on the Cloud' },
+      resolve: {
+        loadPlugin: function ($ocLazyLoad) {
+          return $ocLazyLoad.load([
+            {
+              serie: true,
+              files: [
+                'bower_components/codemirror/lib/codemirror.css',
+                'bower_components/codemirror/theme/ambiance.css',
+                'bower_components/codemirror/lib/codemirror.js',
+                'bower_components/codemirror/mode/javascript/javascript.js'
+              ]
+            },
+            {
+              name: 'ui.codemirror',
+              files: ['bower_components/angular-ui-codemirror/ui-codemirror.min.js']
+            }
+          ]);
+        }
+      }
+    })
     .state('labs.introduction3', {
       url: "/introduction3",
       templateUrl: "views/labs/introduction/index3.html",
