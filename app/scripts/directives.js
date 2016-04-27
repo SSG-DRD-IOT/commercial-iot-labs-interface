2dd4db3f85062c09fc03a17cc46efa19726695fb
Daniel Holmlund <agnathan@gmail.com>
2016-04-27 13:44:25 -0700

    Flattening more labs

--- app/scripts/directives.js
+++ app/scripts/directives.js
@@ -157,7 +157,7 @@ function iboxTools($timeout) {
 function minimalizaSidebar($timeout) {
     return {
         restrict: 'A',
-        template: '<a class="navbar-minimalize minimalize-styl-2 btn btn-primary " href="" ng-click="minimalize()"><i class="fa fa-bars"></i></a>',
+        template: '<a class="navbar-minimalize minimalize-styl-2 btn btn-primary " href="" ng-click="minimalize()"><i class="fa fa-arrow-left"></i></a>',
         controller: function ($scope, $element) {
             $scope.minimalize = function () {
                 $("body").toggleClass("mini-navbar");
