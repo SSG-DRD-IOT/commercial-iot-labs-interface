82017c8eac4e309265d261c9400577e55637a732
Daniel Holmlund <agnathan@gmail.com>
2017-02-02 11:04:39 -0800

    Moving files around

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,7 +23,7 @@ module.exports = function (grunt) {
         connect: {
             options: {
                 port: 9000,
-                hostname: '192.168.3.176',
+                hostname: '127.0.0.1',
                 livereload: 35729
             },
             livereload: {
